fd57664cf1a6e5c688e045997857eb0ab5d111ea
jyip01 <64282396+jyip01@users.noreply.github.com>
2020-09-15 11:45:41 -0400

    Add files via upload

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 
+const config = require('./src/config');
+
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
   "role": "jyip",
   "db": "noteful_test",
-  "connectionString": (process.env.NODE_ENV === 'test')
+  /*"connectionString": (process.env.NODE_ENV === 'test')
     ? "postgresql://jyip:1234@localhost/noteful_test"
-    : "postgresql://dunder_mifflin:password@localhost/noteful",
+    : DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:password@localhost/noteful',*/
+    "connectionString": (process.env.NODE_ENV === 'test') 
+    ? config.TEST_DATABASE_URL 
+    : config.DATABASE_URL,
 }
--- src/server.js
+++ src/server.js
@@ -4,12 +4,13 @@ const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
   client:'pg',
-  connection: {
+  connection: DATABASE_URL,
+  /*connection: {
     host : '127.0.0.1',
     user : 'dunder_mifflin',
     password : 'password',
     database : 'noteful'
-  }
+  }*/
 })
 app.set('db',db)
 
